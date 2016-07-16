965b964c2cac3eeed112efca5767fa277233bd83
nnnn20430 <nnnn20430@mindcraft.si.eu.org>
2016-07-16 19:05:26 +0200

    Update 2016-07-16T17:05:40.931Z

--- misc/runInBg.sh
+++ misc/runInBg.sh
@@ -1,17 +1,25 @@
 # source this file
 
 runInBg() {
-	echo -n $(
+	local SETSID=()
+	if command -v perl >/dev/null 2>&1; then
+		SETSID=('perl' '-e' 'use POSIX;POSIX::setsid();exec @ARGV' '--')
+	elif command -v python >/dev/null 2>&1; then
+		SETSID=('python' '-c'
+		'import os,sys;os.setsid();os.execvp(sys.argv[1],sys.argv[1:])')
+	elif command -v setsid >/dev/null 2>&1; then
+		SETSID=('setsid')
+	fi
+	echo $(
 		(
 			[[ -t 0 ]] && exec </dev/null
 			[[ -t 1 ]] && exec >/dev/null
 			[[ -t 2 ]] && exec 2>/dev/null
+			trap '' 1 2 
 			eval exec {3..255}\>\&-
-			trap '' 1 2
-			cd "/"
-			umask 0
-			exec perl -e 'use POSIX;POSIX::setsid();exec @ARGV' -- "$@"
+			umask 0022
+			exec "${SETSID[@]}" "$@"
 		) >/dev/null 2>&1 &
-		echo -n $!
+		echo $!
 	)
 }
